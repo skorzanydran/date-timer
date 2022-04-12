74b39a062632500653d05b07aeada1e34c04b1ca
v.shevchik <v.shevchik@evotor.ru>
2022-04-12 04:43:35 +0300

    minor

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 var countDownDate = new Date("Apr 23, 2022 04:25:10").getTime();
 
-var x = setInterval(function() {
+var x = setInterval(function () {
+
+  var d = 1000 * 60 * 60 * 24;
+  var h = 1000 * 60 * 60;
+  var s = 1000 * 60;
 
   var now = new Date().getTime();
   var distance = countDownDate - now;
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  var days = Math.floor(distance / d);
+  var hours = Math.floor((distance % d) / h);
+  var minutes = Math.floor((distance % h) / s);
+  var seconds = Math.floor((distance % s) / 1000);
 
-  document.getElementById("demo").innerHTML = days + " дней " + hours + " часов "
+  document.getElementById("demo").innerHTML = days + " дней " + hours
+      + " часов "
       + minutes + " минут " + seconds + " секунд ";
 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    document.getElementById("demo").innerHTML = "Вы на месте!";
   }
 }, 1000);
